f52c5000b54b61af764bf7573a983eaf8aa529a5
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2023-10-05 02:20:07 +0800

    add listcar component

---
+++ src/Components/GetCar/ListCar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Car from "./Car";
+const ListCar = ({ Cars }) => {
+  return (
+    <div>
+      {Cars.map((car, i) => (
+        <ul>
+          <Car key={car.id} car={car} />
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default ListCar;
