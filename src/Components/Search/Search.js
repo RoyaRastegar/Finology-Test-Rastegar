66c851960be6e6f8c5d22fef142c1636f8e7cc49
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2023-10-05 11:06:55 +0800

    update search component

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -5,7 +5,6 @@ const Search = ({ quary, setQuary }) => {
     <div className="search ">
       <label>Search Car Type:</label>
       <select value={quary} onChange={(e) => setQuary(e.target.value)}>
-        <option value="all">All</option>
         <option value="electrical">Electrical</option>
         <option value="sedan">Sedan</option>
         <option value="sport">Sport</option>
